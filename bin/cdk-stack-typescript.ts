76d98de90b6bc3f43d8e355802d985df17813cd9
Samuel Molling <samuel.molling@ifood.com.br>
2024-08-05 09:11:43 -0300

    test

--- bin/cdk-stack-typescript.ts
+++ bin/cdk-stack-typescript.ts
@@ -10,6 +10,5 @@ const app = new cdk.App();
 new TrustStack(app, "TrustStack", {});
 new LambdaStack(app, "LambdaStack", {});
 
-
 // AWS_PROFILE=<seu-perfil> npx cdk deploy TrustStack --parameters GitHubOrg=SamuelMolling --parameters GitHubRepo=cdk-stack-typescript
 // arn:aws:iam::380561476517:role/TrustStack-GitHubActionsRole4F1BBA26-bim7O0v9NUxd 
